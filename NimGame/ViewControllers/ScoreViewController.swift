d3d106d3f20a58bf5160fa9604892bc98ab76eff
laurent <laurent.esperance@gmail.com>
2016-11-18 16:48:13 +0100

    ADD delete function to score list

--- NimGame/ViewControllers/ScoreViewController.swift
+++ NimGame/ViewControllers/ScoreViewController.swift
@@ -9,10 +9,12 @@
 import UIKit
 
 class ScoreViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
-
+    
     var playerListNames: [String] = []
     var playerListScores : [Int] = []
     var sortedKeys:[(key:String, value:Int)] = []
+    var playerDictonary:[String : Int] = [:]
+    
     
     @IBOutlet weak var ui_scoresTableView: UITableView!
     
@@ -20,21 +22,25 @@ class ScoreViewController: UIViewController,UITableViewDelegate, UITableViewData
         super.viewDidLoad()
         ui_scoresTableView.delegate = self
         ui_scoresTableView.dataSource = self
-        
-        let playerDictonary = SettingsManager.instance.playerList
-        
+        self.navigationItem.rightBarButtonItem = self.editButtonItem
+        playerDictonary = SettingsManager.instance.playerList
         sortedKeys = playerDictonary.sorted{ $0.value > $1.value }
         print(sortedKeys)
-        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func ui_backButtonfromScoreList() {
-        dismiss(animated: true, completion: nil)
+    /*@IBAction func editScoreTable(_ sender: UIBarButtonItem) {
+     self.isEditing = !self.isEditing
+     }*/
+    
+    
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        self.ui_scoresTableView.setEditing(editing, animated: animated)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -52,18 +58,31 @@ class ScoreViewController: UIViewController,UITableViewDelegate, UITableViewData
         return cellc
     }
     
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            let keyInDict:String = sortedKeys[indexPath.row].key
+            print("The key \(keyInDict) will be removed from memory")
+            playerDictonary.removeValue(forKey: keyInDict)
+            SettingsManager.instance.playerList = playerDictonary
+            sortedKeys.remove(at: indexPath.row)
+            tableView.deleteRows(at:[indexPath], with: .fade)
+        }
+    }
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-       
+        let keyInDict:String = sortedKeys[indexPath.row].key
+        print("The key \(keyInDict) is selected")
     }
     
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
